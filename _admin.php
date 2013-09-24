e050fdb09a03934111ee5d72107bceb2c82d7a60
franck <carnet.franck.paul@gmail.com>
2013-09-24 13:39:38 +0200

    locales

--- _admin.php
+++ _admin.php
@@ -12,6 +12,9 @@
 
 if (!defined('DC_CONTEXT_ADMIN')) { return; }
 
+// dead but useful code, in order to have translations
+__('Ordering category entries').__('Set category entries order');
+
 $_menu['Blog']->addItem(__('Categories orders'),'plugin.php?p=catOrder','index.php?pf=catOrder/icon.png',
 		preg_match('/plugin.php\?p=catOrder(&.*)?$/',$_SERVER['REQUEST_URI']),
 		$core->auth->check('admin',$core->blog->id));
