4cae0a852af1e6ced554286fceef5956c8d23e00
franck <carnet.franck.paul@gmail.com>
2022-12-18 11:07:11 +0100

    Release 1.1

--- _define.php
+++ _define.php
@@ -18,7 +18,7 @@ $this->registerModule(
     'Ordering category entries',
     'Set category entries order',
     'Franck Paul',
-    '1.0',
+    '1.1',
     [
         'requires'    => [['core', '2.24']],
         'permissions' => dcCore::app()->auth->makePermissions([
