17794e9204a869605f779ed9a345c3b8f18d3b3a
franck <carnet.franck.paul@gmail.com>
2025-09-12 10:18:27 +0200

    Fix test on counter per categories

--- src/FrontendBehaviors.php
+++ src/FrontendBehaviors.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @brief catOrder, a plugin for Dotclear 2
  *
@@ -24,42 +25,49 @@ class FrontendBehaviors
      */
     public static function coreBlogBeforeGetPosts(ArrayObject $params): string
     {
-        if (App::url()->getType() == 'category') {
+        if (App::url()->getType() === 'category') {
             $settings = My::settings();
             if ($settings->active) {
                 $cat_id = App::frontend()->context()->categories->cat_id;
                 $orders = $settings->orders;
-                if (is_array($orders) && array_key_exists($cat_id, $orders) && $orders[$cat_id] != '') {
-                    // Specific order set for the category
-                    switch ($orders[$cat_id]) {
-                        case 'asc':
-                        case 'desc':
-                            $params->offsetSet('order', 'post_dt');
-
-                            break;
-                        case 'title-asc':
-                        case 'title-desc':
-                            $params->offsetSet('order', App::con()->lexFields('post_title'));
-
-                            break;
+                if (is_array($orders) && array_key_exists($cat_id, $orders) && $orders[$cat_id] !== '') {
+                    // Do not sort other entry types
+                    if ($params->offsetExists('post_type') && $params['post_type'] !== 'post') {
+                        return '';
                     }
+                    // Do not sort if order not present in parameters
+                    if ($params->offsetExists('order')) {
+                        // Specific order set for the category
+                        switch ($orders[$cat_id]) {
+                            case 'asc':
+                            case 'desc':
+                                $params->offsetSet('order', 'post_dt');
 
-                    switch ($orders[$cat_id]) {
-                        case 'asc':
-                        case 'title-asc':
-                            $params->offsetSet('order', $params['order'] . ' asc');
+                                break;
+                            case 'title-asc':
+                            case 'title-desc':
+                                $params->offsetSet('order', App::db()->con()->lexFields('post_title'));
 
-                            break;
-                        case 'desc':
-                        case 'title-desc':
-                            $params->offsetSet('order', $params['order'] . ' desc');
+                                break;
+                        }
 
-                            break;
+                        switch ($orders[$cat_id]) {
+                            case 'asc':
+                            case 'title-asc':
+                                $params->offsetSet('order', $params['order'] . ' asc');
+
+                                break;
+                            case 'desc':
+                            case 'title-desc':
+                                $params->offsetSet('order', $params['order'] . ' desc');
+
+                                break;
+                        }
                     }
                 }
 
                 $numbers = $settings->numbers;
-                if (is_array($numbers) && array_key_exists($cat_id, $numbers) && $numbers[$cat_id] != '') {
+                if (is_array($numbers) && array_key_exists($cat_id, $numbers) && (int) $numbers[$cat_id] > 0) {
                     // Specific number of entry per page set for the category
                     App::frontend()->context()->nb_entry_per_page = (int) $numbers[$cat_id];
                 }
