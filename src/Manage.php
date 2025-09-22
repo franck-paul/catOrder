a0692c1c215e001e86d602210e98de5918179c1b
franck <carnet.franck.paul@gmail.com>
2025-09-22 10:10:58 +0200

    Use backend services rather than static calls

--- src/Manage.php
+++ src/Manage.php
@@ -16,8 +16,6 @@ declare(strict_types=1);
 namespace Dotclear\Plugin\catOrder;
 
 use Dotclear\App;
-use Dotclear\Core\Backend\Notices;
-use Dotclear\Core\Backend\Page;
 use Dotclear\Helper\Html\Form\Checkbox;
 use Dotclear\Helper\Html\Form\Form;
 use Dotclear\Helper\Html\Form\Hidden;
@@ -84,7 +82,7 @@ class Manage
 
                 App::blog()->triggerBlog();
 
-                Notices::addSuccessNotice(__('Settings have been successfully updated.'));
+                App::backend()->notices()->addSuccessNotice(__('Settings have been successfully updated.'));
                 My::redirect();
             } catch (Exception $e) {
                 App::error()->add($e->getMessage());
@@ -174,15 +172,15 @@ class Manage
                     ->items($raws));
         }
 
-        Page::openModule(My::name());
+        App::backend()->page()->openModule(My::name());
 
-        echo Page::breadcrumb(
+        echo App::backend()->page()->breadcrumb(
             [
                 Html::escapeHTML(App::blog()->name()) => '',
                 __('Categories entry orders')         => '',
             ]
         );
-        echo Notices::getNotices();
+        echo App::backend()->notices()->getNotices();
 
         // Form
 
@@ -207,6 +205,6 @@ class Manage
             ])
         ->render();
 
-        Page::closeModule();
+        App::backend()->page()->closeModule();
     }
 }
