9bd3ccb79f30b13ea40f183cb9e10bdd22d8e399
franck <contact@open-time.net>
2026-06-27 11:48:20 +0200

    Review icon colors (DC 2.39+)

--- _define.php
+++ _define.php
@@ -18,7 +18,7 @@ $this->registerModule(
     '6.2',
     [
         'date'        => '2026-04-11T14:19:03+0200',
-        'requires'    => [['core', '2.36']],
+        'requires'    => [['core', '2.39']],
         'permissions' => '',
         'type'        => 'plugin',
         'settings'    => [
